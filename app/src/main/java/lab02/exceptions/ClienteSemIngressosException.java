b394c4541026f4936fee469b46c8641b6239a446
minhoslucas <lucasbeserrafernandes@gmail.com>
2025-05-05 00:01:57 -0300

    main finalizada e alterações em certas classes para se adequar ao enunciado

---
+++ app/src/main/java/lab02/exceptions/ClienteSemIngressosException.java
@@ -0,0 +1,7 @@
+package lab02.exceptions;
+
+public class ClienteSemIngressosException extends Exception {
+    public ClienteSemIngressosException(String errorMessage){
+        super(errorMessage);
+    }
+}
